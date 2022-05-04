e39ae71bd3c4d88c54fac88a4b3795f8a2cbdee2
jylim <imjee02@gmail.com>
2022-05-04 10:45:20 +0900

    add bean-maker

---
+++ converter.js
@@ -0,0 +1,64 @@
+
+var main = {
+
+    
+    init : function() {
+
+        var _this = this;
+        $('#btn-bean').on('click' , function() {
+            
+            _this.makeBean();
+
+        })
+        
+
+    },
+    
+    makeBean : function() {
+        
+        var _this = this;
+        var input = $('#input-text').val().toLowerCase().split('\n');
+        
+        for(var i=0 ; i<input.length;i++) {
+            
+            var str = input[i];
+            while(true) {
+                
+                var num = str.indexOf('_')+1;
+                
+                if (num==0) break;
+                
+                var alp = str[num];
+                str = str.replaceAll('_'+alp , alp.toUpperCase()); 
+
+            }
+            input[i]= "private String " + str;
+
+        }
+        
+        var output =_this.makeSen(input);
+        $('#output-text').val(output);
+        
+    }
+
+    , makeSen : function(input) {
+
+        var res = "";
+        for(var i=0; i<input.length ;i++) {
+
+            res+=input[i] + '\n';
+
+        }
+        
+        return res;
+
+
+    }
+
+
+
+
+
+}
+
+main.init();
